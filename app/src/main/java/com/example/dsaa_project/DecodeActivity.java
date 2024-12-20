e51f6d1ff9cdba005c27e16696cbfef8aa2aef80
Dr-helicopter <hashmya1@gmail.com>
2024-12-20 16:57:56 +0330

    got the options working

--- app/src/main/java/com/example/dsaa_project/DecodeActivity.java
+++ app/src/main/java/com/example/dsaa_project/DecodeActivity.java
@@ -1,9 +1,10 @@
 package com.example.dsaa_project;
 
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,10 +13,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 import classes.Decoder;
 
 public class DecodeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,26 +28,56 @@ public class DecodeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add("prefix");
+        options.add("infix");
+        options.add("postfix");
+        this.spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                options
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
+
+
+
         Button decode = findViewById(R.id.decode_button);
-        decode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                decode();
-            }
-        });
+        decode.setOnClickListener(v -> decode());
     }
 
+
+    private Spinner spinner;
     private void decode(){
+        String selectedValue = spinner.getSelectedItem().toString();
+
+
         EditText inputBox = findViewById(R.id.input_text_box);
         String input = inputBox.getText().toString();
-        String answere;
-        try {
-            answere = String.valueOf(Decoder.decodePrefix(input));
-        } catch (Exception e){
-            answere = "Syntax error";
+
+        String answer = " ";
+        switch (selectedValue){
+            case "prefix":
+                try { answer = String.valueOf(Decoder.decodePrefix(input));}
+                catch (Exception e){ answer = "Syntax error"; }
+                break;
+            case "infix":
+                answer = "infix";
+                break;
+            case "postfix":
+                answer = "postfix";
+                break;
         }
+
         TextView answereBox = findViewById(R.id.answere_text);
-        answereBox.setText("the answer is \n" + answere);
+        answereBox.setText("the answer is \n" + answer);
     }
 
 
